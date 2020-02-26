afabb52bf5b6b7230e15e75176f262edba85aab6
Jonathan Prescott-Roy <jjpr@mit.edu>
2020-02-26 15:13:01 -0500

    beginning changes for removing the xarray inplace argument, which is deprecated.

--- brainio_base/assemblies.py
+++ brainio_base/assemblies.py
@@ -22,7 +22,6 @@ class DataAssembly(DataArray):
 
     def __init__(self, *args, **kwargs):
         super(DataAssembly, self).__init__(*args, **kwargs)
-        gather_indexes(self)
 
     def multi_groupby(self, group_coord_names, *args, **kwargs):
         multi_group_name = "multi_group"
@@ -210,7 +209,7 @@ def gather_indexes(xr_data):
         if coords:
             coords_d[dim] = list(coords.keys())
     if coords_d:
-        xr_data.set_index(append=True, inplace=True, **coords_d)
+        return xr_data.set_index(append=True, **coords_d)
     return xr_data
 
 
@@ -244,8 +243,8 @@ class GroupbyBridge(object):
         split_coords = list(map(list, zip(*split_coords)))  # transpose
         for coord_name, coord in zip(self.group_coord_names, split_coords):
             result.coords[coord_name] = (self.multi_group_name, coord)
-        result.reset_index(self.multi_group_name, drop=True, inplace=True)
-        result.set_index(append=True, inplace=True, **{self.multi_group_name: list(self.group_coord_names)})
+        result = result.reset_index(self.multi_group_name, drop=True)
+        result = result.set_index(append=True, **{self.multi_group_name: list(self.group_coord_names)})
         result = result.rename({self.multi_group_name: self.dim})
         return result
 
